5bfeca7d2fb7e551f827871fcec83c364aeefd13
jaclynpqc <pham02@allegheny.edu>
2024-02-28 21:37:21 -0500

    feat: initial convert json to csv using pandas

---
+++ scripts/automl/runAutoML.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Read the JSON file into a DataFrame
+json_file_path = "testFile.json"
+df = pd.read_json(json_file_path)
+
+# Convert DataFrame to CSV and save it
+csv_file_path = 'testFile.csv'
+df.to_csv(csv_file_path, index = False)
+
+print(f"Succesfully convert {json_file_path} to {csv_file_path}. Begin training the model...")
