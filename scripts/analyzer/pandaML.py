ca503628473c16a6f388a27af83b84976a9d5b44
CalebKendra <calebkendra7@gmail.com>
2024-02-29 18:30:23 -0500

    added base panda and sapientml model

--- scripts/analyzer/pandaML.py
+++ scripts/analyzer/pandaML.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sapientml import SapientML
 import json
 
 
@@ -6,12 +9,26 @@ def pandaJSON(data):
 	with open(data) as file:    
 		json_data = json.load(file) 
 	df = pd.json_normalize(json_data)
+	df["mutation_score"] = (df["mutmut_summary.tests"]-df["mutmut_summary.failures"]) / df["mutmut_summary.tests"]
 	return df
+
+
+def sapient(dataframe):
+	cls = SapientML(
+	    target_columns=["mutation_score"],
+	    task_type=None, 
+	)
+	dataframe, test_data = train_test_split(dataframe)
+	y_true = test_data["mutation_score"].reset_index(drop=True)
+	test_data.drop("mutation_score", axis=1, inplace=True)
+	cls.fit(dataframe, output_dir="./outputs")
+	y_pred = cls.predict(test_data)
+	return f1_score(y_true,y_pred)
 
 
 def main():
 	frame = pandaJSON("combined_result.json")
-	print(frame)
+	print(sapient(frame))
 
 
 if __name__ == '__main__':
