82ee5842d44128e667bcea3ad9ec77329c4770c5
jaclynpqc <pham02@allegheny.edu>
2024-02-28 21:39:48 -0500

    feat: flatten nested structure and display

--- scripts/automl/runAutoML.py
+++ scripts/automl/runAutoML.py
@@ -3,6 +3,9 @@ import pandas as pd
 # Read the JSON file into a DataFrame
 json_file_path = "testFile.json"
 df = pd.read_json(json_file_path)
+df_flattened = pd.json_normalize(df,sep='_')
+#Display the DF
+print(df_flattened.head())
 
 # Convert DataFrame to CSV and save it
 csv_file_path = 'testFile.csv'
