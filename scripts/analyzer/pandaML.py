8f9580718df3de8b0829738c9dd27569cb684c7e
CalebKendra <calebkendra7@gmail.com>
2024-02-29 18:38:29 -0500

    revert changes

--- scripts/analyzer/pandaML.py
+++ scripts/analyzer/pandaML.py
@@ -1,7 +1,4 @@
 import pandas as pd
-from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
-from sapientml import SapientML
 import json
 from automl.sapientml.sapientml.main import SapientML
 from sklearn.model_selection import train_test_split
@@ -12,14 +9,9 @@ def load_data(data):
     df = pd.json_normalize(json_data)
     return df
 
-
-def pandaJSON(data):
-	with open(data) as file:    
-		json_data = json.load(file) 
-	df = pd.json_normalize(json_data)
-	df["mutation_score"] = (df["mutmut_summary.tests"]-df["mutmut_summary.failures"]) / df["mutmut_summary.tests"]
-	return df
-
+def define_mutation_score(mutant_data)->int:
+    # Define the 'mutation_score' based on the presence of 'failure' in 'mutants'
+    return 1 if 'failure' in mutant_data else 0
 
 def train_and_evaluate_model(data):
     # Load data
@@ -27,12 +19,11 @@ def train_and_evaluate_model(data):
 
     # Extract features and define 'mutation_score'
     # Features are the inputs variable for the model, 'mutation_score" is the label we want to predict
-    features = frame.drop(columns=['mutants']) # Drop column as it's not a feature.
+    features = frame.drop(columns=['mutants']) # Drop column as it's not a feature
     frame['mutation_score'] = frame['mutants'].apply(define_mutation_score) # Define `mutation_score` from function above
     labels = frame['mutation_score']
 
     # Split the data into training and testing sets, assess the model's performance on unseen data
-    # So dividing our dataset into two subsets (`X_train`, `Y_train`) and (`X_test`,`Y_test`)
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
     # Initialize AutoML
@@ -51,23 +42,8 @@ def train_and_evaluate_model(data):
     print("Feature Importance:")
     print(feature_importance)
 
-def sapient(dataframe):
-	cls = SapientML(
-	    target_columns=["mutation_score"],
-	    task_type=None, 
-	)
-	dataframe, test_data = train_test_split(dataframe)
-	y_true = test_data["mutation_score"].reset_index(drop=True)
-	test_data.drop("mutation_score", axis=1, inplace=True)
-	cls.fit(dataframe, output_dir="./outputs")
-	y_pred = cls.predict(test_data)
-	return f1_score(y_true,y_pred)
-
-
 def main():
-	frame = pandaJSON("combined_result.json")
-	print(sapient(frame))
-
+    train_and_evaluate_model("combined_result.json")
 
 if __name__ == '__main__':
     main()
