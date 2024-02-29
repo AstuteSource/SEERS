a48f15c64c666e35de2991e813cd62b5a6f66268
jaclynpqc <pham02@allegheny.edu>
2024-02-28 21:47:02 -0500

    fix: delete flatten

--- scripts/automl/runAutoML.py
+++ scripts/automl/runAutoML.py
@@ -3,12 +3,11 @@ import pandas as pd
 # Read the JSON file into a DataFrame
 json_file_path = "testFile.json"
 df = pd.read_json(json_file_path)
-df_flattened = pd.json_normalize(df,sep='_')
 #Display the DF
-print(df_flattened.head())
+print(df.head())
 
 # Convert DataFrame to CSV and save it
 csv_file_path = 'testFile.csv'
 df.to_csv(csv_file_path, index = False)
 
-print(f"Succesfully convert {json_file_path} to {csv_file_path}. Begin training the model...")
+print(f"Successfully convert {json_file_path} to {csv_file_path}. Begin training the model...")
