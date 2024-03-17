a0f3be9b6a90f5e1ee0666df67740ea03b2ca137
jaclynpqc <pham02@allegheny.edu>
2024-03-17 18:39:25 -0400

    feat: delete unwanted import

--- scripts/analyzer/main.py
+++ scripts/analyzer/main.py
@@ -1,3 +1,4 @@
+
 """Program to execute and store Chasten and Mutmut analysis for lazytracker"""
 
 import typer
@@ -10,12 +11,9 @@ from glob import glob
 from contextlib import chdir
 from rich.console import Console
 from pathlib import Path
-from json_restruct import json_restruct
-from add_function_json import add_function_to_json
 from combined import save_output, restructure_and_add_function_info, load_json_data
 
 cli = typer.Typer()
-
 
 def install_package(package):
     """Install the specified Python package using pip."""
@@ -45,7 +43,7 @@ def execute_chasten(search_path, save_directory, chasten_config_path):
     chasten_command = [
         "chasten",
         "analyze",
-        "ultiplayground",
+        "lazytracker",
         "--config",
         chasten_config_path,
         "--search-path",
@@ -63,6 +61,7 @@ def execute_chasten(search_path, save_directory, chasten_config_path):
         file = json.load(f)
     os.remove(json_name)
     return file
+
 
 
 def execute_mutmut(search_path):
@@ -98,7 +97,7 @@ def save_results(chasten_result, mutmut_result, save_file):
 
 @cli.command()
 def analyzer(
-    search_path: Path = os.getcwd() + "/demo/python-playground",
+    search_path: Path = os.getcwd() + "/demo/lazytracker",
     save_directory: Path = os.path.abspath(os.path.dirname(__file__)),
     chasten_config_path: str = os.getcwd() + "/Config",
 ):
@@ -127,4 +126,3 @@ def analyzer(
         console.print("Result is stored in file named combined_result.json")
         console.print(":broom: Final sweeping, saved to new_output_with_functions.json")
         save_output(restructure_and_add_function_info(load_json_data('combined_result.json'), search_path), 'new_output_with_functions.json')
-
